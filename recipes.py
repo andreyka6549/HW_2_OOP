9238e2019d7684421b06a8272724fbd753efe398
Антонов Андрей <andromantonov@edu.hse.ru>
2026-06-02 23:47:47 +0300

    feat: add Recipe implementation

--- recipes.py
+++ recipes.py
@@ -1,3 +1,5 @@
+from numbers import Number
+
 class Ingredient:
     def __init__(self, name: str, quantity: float, unit: str) -> None:
         self.name: str = name
@@ -24,3 +26,31 @@ class Ingredient:
         return self.name == other.name and self.unit == other.unit
 
 
+class Recipe:
+    def __init__(self, title: str) -> None:
+        self.title = title
+        self.ingredients: list[Ingredient] = []
+
+    def add_ingredient(self, ingredient: Ingredient) -> None:
+        for other in self.ingredients:
+            if other == ingredient:
+                other.quantity += ingredient.quantity
+                return
+        self.ingredients.append(ingredient)
+
+    @staticmethod
+    def is_valid_ratio(ratio) -> bool:
+        return isinstance(ratio, Number) and ratio > 0
+
+    def scale(self, ratio: float) -> "Recipe": # forward reference https://stackoverflow.com/questions/55320236/does-python-evaluate-type-hinting-of-a-forward-reference первый коммент первое решение
+        if not self.is_valid_ratio(ratio): # иначе зачем мы этот метод определяли?
+            raise ValueError("Ratio должно быть положительным числом")
+        obj: Recipe = Recipe(self.title)
+        for ingredient in self.ingredients:
+            new_ingredient = Ingredient(ingredient.name, ingredient.quantity * ratio, ingredient.unit)
+            obj.add_ingredient(new_ingredient)
+
+        return obj
+
+    def __len__(self) -> int:
+        return len(self.ingredients)
